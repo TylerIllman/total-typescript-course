31fa903eae679e3aa015c1ef3bff0b07fad5f84d
TylerIllman <tyler.illman.productions@gmail.com>
2023-11-09 09:01:08 +0800

    E12

--- src/12-typeof-narrowing.problem.ts
+++ src/12-typeof-narrowing.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-const coerceAmount = (amount: number | { amount: number }) => {};
+const coerceAmount = (amount: number | { amount: number }) => {
+  if (typeof amount === "number") { // By checking the typeof, ts knows what part of hte union operator it is in
+    return amount;
+  }
+  return amount.amount;
+};
 
 it("Should return the amount when passed an object", () => {
   expect(coerceAmount({ amount: 20 })).toEqual(20);
